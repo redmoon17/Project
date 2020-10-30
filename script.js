129a0f538b59888f1540bb5ba096dff1b8aa1418
Sergei <example@gmail.com>
2020-10-30 17:08:49 +0300

    second commit

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 const numberOfFilm = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const personalMovieDB = {
@@ -7,11 +8,29 @@ const personalMovieDB = {
     genres: [],
     privat: false
 };
-const a = prompt('Один из последних просмотреных фильмов?', ''),
-    b = prompt('На сколько вы оцените его?', ''),
-    c = prompt('Один из последних просмотреных фильмов?', ''),
-    d = prompt('На сколько вы оцените его?', '');
-personalMovieDB.movie[a] = b;
-personalMovieDB.movie[c] = d;
+
+for (let i = 0; i < 2; i++) {
+   const a = prompt('Один из последних просмотреных фильмов?', ''),
+         b = prompt('На сколько вы оцените его?', '');
+
+   if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+       personalMovieDB.movie[a] = b;
+       console.log('done');
+   } else {
+       console.log('error');
+       i--;
+   }
+
+   if (personalMovieDB.count < 10){
+       console.log('Просмотрено довольно мало фильмов');
+   } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+       console.log('Вы классический зритель');
+   } else if (personalMovieDB.count >= 30) {
+       console.log('Вы киноман');
+   } else {
+       console.log('Произошла ошибка');
+   }
+
+}
 
 console.log(personalMovieDB);
